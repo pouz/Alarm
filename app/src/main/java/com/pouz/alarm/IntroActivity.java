49fc339aec672ac0d15441bc6470cbae52de5257
PouZ <whitewyrm84@gmail.com>
2017-02-25 13:44:46 +0900

    11

--- app/src/main/java/com/pouz/alarm/IntroActivity.java
+++ app/src/main/java/com/pouz/alarm/IntroActivity.java
@@ -12,33 +12,14 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.pouz.alarm.alarms.AlarmsActivity;
-import com.pouz.alarm.receiver.DummyCallReceiver;
 
 public class IntroActivity extends AppCompatActivity
 {
-    Intent mIntent;
-    Context mContext;
-    AlarmManager mAlarmManager;
-    PendingIntent mPendingIntent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-        Log.i("IntroActivity", "onCreate() -> Make PendingIntent for calling DummyService");
-        Toast.makeText(this, "IntroActivity : onCreate() -> Make PendingIntent for calling DummyService", Toast.LENGTH_SHORT);
-
-        mContext = this;
-        mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
-
-        mIntent = new Intent(mContext, DummyCallReceiver.class);
-        mIntent.setAction(DummyCallReceiver.START_DUMMY_SERVICE);
-
-        mPendingIntent.getBroadcast(mContext, 0, mIntent, 0);
-        /** 10초에 한번씩 DummyCallReceiver 호출 */
-        mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 1000, 10000, mPendingIntent);
 
         (new Handler()).postDelayed(new Runnable()
         {
--- app/src/main/java/com/pouz/alarm/receiver/BootReceiver.java
+++ app/src/main/java/com/pouz/alarm/receiver/BootReceiver.java
@@ -13,24 +13,24 @@ import com.pouz.alarm.service.DummyService;
 
 public class BootReceiver extends BroadcastReceiver
 {
-    Intent mIntent;
-    Context mContext;
-    AlarmManager mAlarmManager;
-    PendingIntent mPendingIntent;
+    //Intent mIntent;
+    //Context mContext;
+    //AlarmManager mAlarmManager;
+    //PendingIntent mPendingIntent;
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
         Log.i("BootReceiver", "onReceive() -> Make PendingIntent for calling DummyService");
-        Toast.makeText(context, "IntroActivity : onReceive() -> Make PendingIntent for calling DummyService", Toast.LENGTH_SHORT);
-        mContext = context;
-        mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+        //Toast.makeText(context, "IntroActivity : onReceive() -> Make PendingIntent for calling DummyService", Toast.LENGTH_SHORT);
+        //mContext = context;
+        //mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
 
-        mIntent = new Intent(mContext, DummyCallReceiver.class);
-        intent.setAction(DummyCallReceiver.START_DUMMY_SERVICE);
+        //mIntent = new Intent(mContext, DummyCallReceiver.class);
+        //intent.setAction(DummyCallReceiver.START_DUMMY_SERVICE);
 
-        mPendingIntent.getBroadcast(mContext, 0, mIntent, 0);
+        //mPendingIntent.getBroadcast(mContext, 0, mIntent, 0);
         /** 10초에 한번씩 DummyCallReceiver 호출 */
-        mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 1000, 10000, mPendingIntent);
+        //mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 1000, 10000, mPendingIntent);
     }
 }
